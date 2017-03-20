4b577d7643220b5415386b806ce07b457a7786ae
henry-rios <henryjhonrioslicito1@gmail.com>
2017-03-20 00:44:08 -0500

    solucion3

--- WcfServiceLibrary1/WcfServiceLibrary1/IService1.cs
+++ WcfServiceLibrary1/WcfServiceLibrary1/IService1.cs
@@ -15,32 +15,6 @@ namespace WcfServiceLibrary1
         string GetData(int value);
         String getDate();
 
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
-        // TODO: agregue aquí sus operaciones de servicio
     }
 
-    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
-    // Puede agregar archivos XSD al proyecto. Después de compilar el proyecto, puede usar directamente los tipos de datos definidos aquí, con el espacio de nombres "WcfServiceLibrary1.ContractType".
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
-    }
 }
